3683b7ecac3b6cdc8d407ff468ae9b289d01ca98
Eddie Rolls <edwardjrolls@gmail.com>
2020-12-31 19:22:20 +0000

    v1 Minesweeper (Human player only)

---
+++ board.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 15:29:35 2020
+
+@author: edwar
+
+TODO:
+    * Implement placing a mine (make sure that it hasn't been guessed)
+    * Create a game object (probably seperate file)
+"""
+import random
+
+class Board:
+    """ The board on which the game is played"""
+    
+    def __init__(self,x=10,y=10,m=10,x0=0,y0=0):
+        """ setup a board of size x by y, with m mines and initial guess (x0,y0) """
+        if x<0 or y<0 or x*y<=m:
+            raise ValueError("Invalid board dimensions")
+        
+        self.dim = (x,y)
+        self.allSquares = set([(a,b) for a in range(x) for b in range(y)])
+        self.uncovered = [] # Tuples (i,j,n) for the square (i,j) and the number of adjacent mines n
+        self.mined = set()
+        self.mines = self.__setupBoard__(m,x0,y0)
+        self.finished = False
+        self.lost = False
+        self.guessSquare(x0,y0)
+    
+    def __setupBoard__(self,m,x0,y0):
+        """ Sets up the board """
+        x,y = self.dim
+        mines = random.sample(range(x*y),m+1)
+        mines = [(m//x,m%y) for m in sorted(mines)]
+        if (x0,y0) in mines:
+            mines.remove((x0,y0))
+        else:
+            mines = mines[:m]
+        return set(mines)
+    
+    def __str__(self):
+        """ Prints the state of the board """
+        s = []
+        uncovered = [(a[0],a[1]) for a in self.uncovered]
+        for j in range(self.dim[1]-1,-1,-1):
+            for i in range(self.dim[0]):
+                if (i,j) in self.mined:
+                    s.append("X")
+                elif (i,j) in uncovered and (i,j) in self.mines:
+                    s.append("M")
+                elif (i,j) in uncovered:
+                    a = uncovered.index((i,j))
+                    if self.uncovered[a][2]==0:
+                        s.append(" ")
+                    else:
+                        s.append(str(self.uncovered[a][2]))
+                else:
+                    s.append("+")
+            s.append("\n")
+        if self.finished and self.lost:
+            s.append("GAME LOST\n")
+        elif self.finished:
+            s.append("GAME WON")
+        return "".join(s)
+        
+    def guessSquare(self,x0,y0):
+        """ Make a guess """
+        if (x0,y0) not in self.allSquares:
+            return 1
+        elif (x0,y0) in [(a[0],a[1]) for a in self.uncovered]:
+            return 2 # Already guessed
+        elif (x0,y0) in self.mined:
+            return 3 # Already mined
+        elif (x0,y0) in self.mines:
+            self.finished = True
+            self.lost = True
+            self.uncovered.append((x0,y0,-1))
+            return 4 # Game lost
+        
+        adjacent = [(x,y) for x in range(x0-1,x0+2) for y in range(y0-1,y0+2)
+                    if (y>=0 and x>=0 and x<self.dim[1] and y<self.dim[1] and not (x==x0 and y==y0))]
+        totalMines = len([a for a in adjacent if a in self.mines])
+        self.uncovered.append((x0,y0,totalMines))
+        if totalMines == 0:
+            for a in adjacent:
+                self.guessSquare(a[0],a[1])
+        
+        if len(self.uncovered) == self.dim[0]*self.dim[1]-len(self.mines):
+            self.finished = True
+            return 5 # Game won!
+        
+        return 0
+        
+        
+    def placeMine(self,x0,y0):
+        """ Place a mine on a square. If already mined then reverse. """
+        if (x0,y0) not in self.allSquares:
+            return 1
+        if (x0,y0) in [(a[0],a[1]) for a in self.uncovered]:
+            return 2 # Already guessed
+        elif (x0,y0) in self.mined:
+            self.mined.remove((x0,y0))
+        else:
+            self.mined.add((x0,y0))
+        
+        return 0 # Already mined
---
+++ game.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 18:22:38 2020
+
+@author: edwar
+TODO:
+    * Create some kind of 'who's playing' option, where selecting a human plater plays what's currently in the init function
+"""
+
+import board
+
+class Game:
+    """ An instance of a game which sets up a board and controls I/O """
+    
+    def __init__(self):
+        """ Currently where the game is played """
+        self.board = self.setupBoard()
+        print(self.board)
+        while not self.board.finished:
+            out = self.makeMove()
+            if out == -1: return -1
+            print(self.board)
+            
+            
+    def setupBoard(self):
+        x,y = parsePair("Select board dimensions as two numbers separated by a space:")
+        m = int(input("How many mines should there be?\n"))
+        x0,y0 = parsePair("What is your initial guess?")
+        bd = board.Board(x,y,m,x0,y0)
+        return bd
+    
+    def makeMove(self):
+        valid = False
+        while not valid:
+            moveType = input("Would you like to place a mine (M), make a guess (G) or quit (Q)\n").upper()
+            if moveType == "Q":
+                return 1
+            elif moveType not in ("G","M"):
+                print("Sorry, that wasn't recognised, please try again.")
+            else:
+                valid = True
+        moveName = "place the mine" if moveType=="M" else "make a guess"
+        x0,y0 = parsePair("Where would you like to "+moveName)
+        if moveType=="G":
+            out = self.board.guessSquare(x0,y0)
+        else:
+            out = self.board.placeMine(x0,y0)
+        
+        if out==1:   print("That square is not on the board")
+        elif out==2: print("That square has already been uncovered")
+        elif out==3: print("That square has a mine on it, please select mine to undo this")
+        elif out==4: print("Commiserations, you have lost!")
+        elif out==5: print("Congratulations, you have won!")
+            
+# Static Methods
+def parsePair(s=""):
+    """ Parse a pair of numbers from input """
+    valid = False
+    while not valid:
+        try:
+            x,y = [int(a) for a in input(s+"\n").split(" ")]
+            valid = True
+        except ValueError:
+            print("Please input two integer co-ordinates separated by a space")
+    return x,y
+
+if __name__ == "__main__":
+    g = Game()
